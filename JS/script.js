7334d01b269736993d4d785dbd9a5e2c2a40861c
Anna <Annabuznitskaya@gmail.com>
2022-02-04 14:53:53 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const answers = [];
+
+answers [0] = prompt ("Как Ваше имя?","");
+answers [1] = prompt ("Как Ваша фамилия?","");
+answers [2] = prompt ("Сколько Вам лет?","");
+
+console.log (answers);
+
+console.log (2 * 4 == '4');
